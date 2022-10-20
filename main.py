2e19a23e2995b135de46e0bbdbfdac989d72c697
Luana Belo <106981076+onniros@users.noreply.github.com>
2022-10-20 00:54:33 +0000

    Inseri as primeiras variáveis

--- main.py
+++ main.py
@@ -3,6 +3,10 @@
 # print() = comando de saída
 print("Hello, world!")
 
-'''
-Comentario em bloco
-'''
+# Guardar String
+nome = "Luana Belo"
+# Guardar int
+idade = 18
+
+# Exibir o conteúdo da variável
+print(nome)
